065252dd4d2f4c50a237b2e25925e50a2686b676
Patvill <107768284+Patvill@users.noreply.github.com>
2024-04-11 15:33:13 -0500

    0.1

---
+++ Foodia/src/main/java/com/upc/foodia/FoodiaApplication.java
@@ -0,0 +1,13 @@
+package com.upc.foodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiaApplication.class, args);
+	}
+
+}
---
+++ Foodia/src/test/java/com/upc/foodia/FoodiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.foodia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
